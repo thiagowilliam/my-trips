3e622acdf97a11defe89386a682e93a6abc4a5de
Thiago William <thiago.will.silva@gmail.com>
2021-05-26 14:48:48 -0300

    finish project

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://my-trips.thiagowilliam.com.br',
+    site_name: 'My Trips'
+  },
+  twitter: {
+    handle: '@thiago_william',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,20 @@
-import Main from 'components/Main'
+import HomeTemplate from 'templates/Home'
+import { MapProps } from 'components/Map'
+import { GET_PLACES } from 'graphql/queries'
+import client from 'graphql/client'
+import { GetPLacesQuery } from 'graphql/generated/graphql'
 
-export default function Home() {
-  return <Main />
+export default function Home({ places }: MapProps) {
+  return <HomeTemplate places={places} />
+}
+
+export const getStaticProps = async () => {
+  const { places } = await client.request<GetPLacesQuery>(GET_PLACES)
+
+  return {
+    revalidate: 60 * 60 * 24, // once per day
+    props: {
+      places
+    }
+  }
 }
